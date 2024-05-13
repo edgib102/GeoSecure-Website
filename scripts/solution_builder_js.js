e80a90ab6fbdc33cdb2ccca9f69aa83a247063ac
edgib102 <58235933+edgib102@users.noreply.github.com>
2024-05-13 21:04:14 +1000

    Solution Builder Logic

--- scripts/solution_builder_js.js
+++ scripts/solution_builder_js.js
@@ -1,5 +1,23 @@
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
+        
+        var optionList = document.getElementsByClassName("optionPanel");
+
+        var selectedOptions = Array(6);
+        
+        for (let i = 0; i < optionList.length; i++) {
+
+
+            optionList[i].addEventListener("click", function() {
+                selectedOptions[i] = optionList[i].getAttribute("data-id")
+                console.log(selectedOptions);
+            })
+            
+        }
+
+
+        document.getElementById("continueBtn")
+
         document.getElementById("mainContent").style.height = "calc(100vh - " + document.getElementById("footerBorder").clientHeight+"px - " + document.getElementById("header").clientHeight+"px)";
         console.log("calc(100vh - " + document.getElementById("footerBorder").clientHeight+"px - " + document.getElementById("header").clientHeight+")");
     }

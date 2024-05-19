d1bcb476d75fc5c7e9070a4b519bb3002a7c0946
edgib102 <58235933+edgib102@users.noreply.github.com>
2024-05-19 21:03:49 +1000

    solution builder work

--- scripts/solution_builder_js.js
+++ scripts/solution_builder_js.js
@@ -1,20 +1,69 @@
-document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        
-        var optionList = document.getElementsByClassName("optionPanel");
+function adjustContentMargin() {
+    const content = document.getElementById('mainContent');
+    
+    const contentHeight = content.offsetHeight;
+    const footerHeight = document.getElementById('footerBorder').offsetHeight;
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const viewportHeight = window.innerHeight;
+    
+    const remainingSpace = viewportHeight - contentHeight - headerHeight - footerHeight;
+    content.style.marginBottom = remainingSpace > 0 ? `${remainingSpace}px` : '0';
+}
 
-        var selectedOptions = Array(6);
-        
-        for (let i = 0; i < optionList.length; i++) {
 
+function initOptions(solutionStep, layoutList){
 
-            optionList[i].addEventListener("click", function() {
-                selectedOptions[i] = optionList[i].getAttribute("data-id")
-                console.log(selectedOptions);
-            })
-            
+    for (let i = 0; i < layoutList.length; i++) {
+        if(i == solutionStep){
+            layoutList[i].style.display = "block";
+            optionList = layoutList[i].querySelectorAll(".optionPanel")
+            optionList.forEach(optionPanel => {
+                optionPanel.addEventListener("click", () =>{
+
+                    switch (solutionStep) {
+                        case 0:
+                            optionList.forEach( optionPanel => {
+                                optionPanel.classList.remove("optionPanel-active")
+                            });
+                                
+                            optionPanel.classList.add("optionPanel-active")
+                            break;
+                        case 1, 2:
+                            optionPanel.classList.toggle("optionPanel-active")
+                        default:
+                            break;
+                    }
+                })
+            });
+        } else{
+            layoutList[i].style.display = "none";
         }
+        
+    }
+}
 
-        document.getElementById("continueBtn")
+document.onreadystatechange = () => {
+    if (document.readyState == "complete") {
+
+        window.addEventListener('resize', adjustContentMargin);
+        window.addEventListener('load', adjustContentMargin);
+        adjustContentMargin();
+
+        let solutionStep = 0;
+
+        const layoutList = document.getElementsByClassName("optionLayout");
+
+        document.querySelectorAll(".continueBtn").forEach(btn => {
+
+            btn.addEventListener("click", () => {
+
+                solutionStep++;
+                console.log(solutionStep)
+                console.log(new Date().toLocaleTimeString())
+                initOptions(solutionStep, layoutList);
+            })
+        })
+
+
     }
 }

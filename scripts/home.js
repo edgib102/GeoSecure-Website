8f4d012b8b5b94daac05fb0a8704659eaf38327f
edgib102 <58235933+edgib102@users.noreply.github.com>
2024-08-10 16:07:45 +1000

    Mobile Fixes

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,7 @@
-elementOffset = 400;
+
 document.onreadystatechange = function () {
 
-
-
     var DDlist = document.getElementsByClassName("headerDD");
-    console.log("DDlist")
     canClick = true;
 
     document.getElementById("DDbg").addEventListener("click", function (e) {
@@ -23,8 +20,6 @@ document.onreadystatechange = function () {
     updateCircleElement(circleElements, lineElementContainer);
     window.onresize = updateCircleElement(circleElements, lineElementContainer)
 
-    centerIcon = document.getElementById("center-icon");
-    centerIcon.style.transform = `translateY(${elementOffset-centerIcon.offsetWidth/2}px)`
 
     window.addEventListener('resize', () =>{
         updateCircleElement(circleElements, lineElementContainer);
@@ -38,7 +33,6 @@ document.onreadystatechange = function () {
 
 function mobileDDtoggle(){
     DDitem = document.getElementById("DDbg");
-    console.log("fgogogogo")
     if(canClick == true){
         if (DDitem.style.display === "block") {
             DDitem.style.display = "none";
@@ -54,52 +48,72 @@ function mobileDDtoggle(){
 
 function updateCircleElement(circleElements, lineElementContainer) {
 
-    while (lineElementContainer.hasChildNodes()){
-        lineElementContainer.removeChild(lineElementContainer.firstChild)
-    }
-    
-    for (let i = 0; i < circleElements.length; i++) {
-        const element = circleElements[i];
-        const circleDisplacement = Math.PI*2/circleElements.length*i
-        translateAmount = [Math.sin(circleDisplacement)*elementOffset, Math.cos(circleDisplacement)*elementOffset]
 
-        element.style.transform = `translate(${translateAmount[0]}px,${(translateAmount[1]+elementOffset)*.85}px)`
-        // console.log(`translate(${translateAmount[0]}px, ${translateAmount[1]}px)`)
+    if(window.innerWidth >= 500){
+        elementOffset = document.getElementById("data-panel").offsetHeight/2 - 110
+        console.log(document.getElementById("data-panel").offsetHeight/2)
 
-
-        const parentPos = lineElementContainer.parentNode.getBoundingClientRect();
-        const childPos  = element.getBoundingClientRect();
-        const relativePos = {};
-
-        relativePos.top    = childPos.top - parentPos.top,
-        relativePos.right  = childPos.right - parentPos.right,
-        relativePos.bottom = childPos.bottom - parentPos.bottom,
-        relativePos.left   = childPos.left - parentPos.left;
-
-        console.log(childPos)
+        while (lineElementContainer.hasChildNodes()){
+            lineElementContainer.removeChild(lineElementContainer.firstChild)
+        }
         
+        for (let i = 0; i < circleElements.length; i++) {
+            const element = circleElements[i];
+            const circleDisplacement = Math.PI*2/circleElements.length*i
+            translateAmount = [Math.sin(circleDisplacement)*elementOffset, Math.cos(circleDisplacement)*elementOffset]
 
-        const lineSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        // lineSVG.setAttribute('style', 'border: 1px solid black');
-        lineSVG.setAttribute('width', "100%");
-        lineSVG.setAttribute('height', lineElementContainer.parentNode.clientHeight);
-        lineSVG.setAttribute('class', "svg-line");
-        
-        lineSVG.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-        lineElementContainer.appendChild(lineSVG);
-        // lineElements.push(lineSVG);
+            element.style.transform = `translate(${translateAmount[0]}px,${(translateAmount[1]+elementOffset)*.85}px)`
 
-        document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
-        newLine.setAttribute('id','line2');
-        newLine.setAttribute('x1',relativePos.left+elementOffset-(element.offsetWidth/2));
-        newLine.setAttribute('y1', relativePos.top+(element.offsetHeight/2));
-        newLine.setAttribute('x2',window.innerWidth/2); 
-        newLine.setAttribute('y2',elementOffset);
-        newLine.setAttribute("stroke", "#0B2149");
-        newLine.setAttribute("stroke-width", "5px")
+            
+            // console.log(`translate(${translateAmount[0]}px, ${translateAmount[1]}px)`)
 
-        lineSVG.appendChild(newLine);
+
+            // const parentPos = lineElementContainer.parentNode.getBoundingClientRect();
+            // const childPos  = element.getBoundingClientRect();
+            // const relativePos = {};
+
+            // relativePos.top    = childPos.top - parentPos.top,
+            // relativePos.right  = childPos.right - parentPos.right,
+            // relativePos.bottom = childPos.bottom - parentPos.bottom,
+            // relativePos.left   = childPos.left - parentPos.left;
+
+            // console.log(childPos)
+            
+
+            // const lineSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            // // lineSVG.setAttribute('style', 'border: 1px solid black');
+            // lineSVG.setAttribute('width', "100%");
+            // lineSVG.setAttribute('height', lineElementContainer.parentNode.clientHeight);
+            // lineSVG.setAttribute('class', "svg-line");
+            
+            // lineSVG.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+            // lineElementContainer.appendChild(lineSVG);
+            // // lineElements.push(lineSVG);
+
+            // document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            // var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+            // newLine.setAttribute('id','line2');
+            // newLine.setAttribute('x1',relativePos.left+elementOffset-(element.offsetWidth/2));
+            // newLine.setAttribute('y1', relativePos.top+(element.offsetHeight/2));
+            // newLine.setAttribute('x2',window.innerWidth/2); 
+            // newLine.setAttribute('y2',elementOffset);
+            // newLine.setAttribute("stroke", "#0B2149");
+            // newLine.setAttribute("stroke-width", "5px")
+
+            // lineSVG.appendChild(newLine);
+        }
+
+        centerIcon = document.getElementById("center-icon");
+        centerIcon.style.transform = `translateY(${elementOffset-centerIcon.offsetHeight/8.5}px)`
+        // centerIcon.style.transform = `translateX(${elementOffset+centerIcon.offsetHeight/2}px)`        
+    } else{
+        // console.log(';ff')
+        for(let i = 0; i < circleElements.length; i++){
+            const element = circleElements[i];
+            element.style.transform = null
+        }
     }
+
+
 }
 

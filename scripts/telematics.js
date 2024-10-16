b6cd98b45389c84918a2f74aa0555752a902266a
edgib102 <edgib102@gmail.com>
2024-10-16 13:52:11 +1000

    Industries Mobile

--- scripts/telematics.js
+++ scripts/telematics.js
@@ -20,43 +20,43 @@ window.addEventListener("load", () => {
     }
 
     
-    const box = document.getElementById('selector-rack');
+    // const box = document.getElementById('selecftor-rack');
 
-    let isDown = false;
-    let startX;
-    let startY;
-    let scrollLeft;
-    let scrollTop;
+    // let isDown = false;
+    // let startX;
+    // let startY;
+    // let scrollLeft;
+    // let scrollTop;
 
-    box.addEventListener('mousedown', (e) => {
-        isDown = true;
-        startX = e.pageX - box.offsetLeft;
-        startY = e.pageY - box.offsetTop;
-        scrollLeft = box.scrollLeft;
-        scrollTop = box.scrollTop;
-        box.style.cursor = 'grabbing';
-    });
+    // box.addEventListener('mousedown', (e) => {
+    //     isDown = true;
+    //     startX = e.pageX - box.offsetLeft;
+    //     startY = e.pageY - box.offsetTop;
+    //     scrollLeft = box.scrollLeft;
+    //     scrollTop = box.scrollTop;
+    //     box.style.cursor = 'grabbing';
+    // });
 
-    box.addEventListener('mouseleave', () => {
-        isDown = false;
-        box.style.cursor = 'grab';
-    });
+    // box.addEventListener('mouseleave', () => {
+    //     isDown = false;
+    //     box.style.cursor = 'grab';
+    // });
 
-    box.addEventListener('mouseup', () => {
-        isDown = false;
-        box.style.cursor = 'grab';
-    });
+    // box.addEventListener('mouseup', () => {
+    //     isDown = false;
+    //     box.style.cursor = 'grab';
+    // });
 
-    document.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - box.offsetLeft;
-        const y = e.pageY - box.offsetTop;
-        const walkX = (x - startX) * 1; // Change this number to adjust the scroll speed
-        const walkY = (y - startY) * 1; // Change this number to adjust the scroll speed
-        box.scrollLeft = scrollLeft - walkX;
-        box.scrollTop = scrollTop - walkY;
-    });
+    // document.addEventListener('mousemove', (e) => {
+    //     if (!isDown) return;
+    //     e.preventDefault();
+    //     const x = e.pageX - box.offsetLeft;
+    //     const y = e.pageY - box.offsetTop;
+    //     const walkX = (x - startX) * 1; // Change this number to adjust the scroll speed
+    //     const walkY = (y - startY) * 1; // Change this number to adjust the scroll speed
+    //     box.scrollLeft = scrollLeft - walkX;
+    //     box.scrollTop = scrollTop - walkY;
+    // });
 
 
 });

fa2313c66602801f87dbd01294859fc8a836ed6d
edgib102 <edgib102@gmail.com>
2024-10-18 16:29:45 +1000

    general touch up

--- scripts/telematics.js
+++ scripts/telematics.js
@@ -3,17 +3,25 @@ window.addEventListener("load", () => {
     solutionPanels = document.querySelectorAll(".solution-panel")
     for (let i = 0; i < thumbs.length; i++) {
         thumbs[i].addEventListener("click", () => {
-            console.log("ejkfoj")
-            document.getElementById("highlighted").removeAttribute("id")
-            thumbs[i].setAttribute('id','highlighted');
 
-            for (let x = 0; x < solutionPanels.length; x++) {
-                const panel = solutionPanels[x];
-                if (thumbs[i].getAttribute("data-id") == panel.getAttribute("data-id")) {
-                    document.getElementById("active").removeAttribute("id")
-                    panel.setAttribute('id','active');
-                }
-            }
+            updateSlide(thumbs[i])
+
+            // document.getElementById("highlighted").removeAttribute("id")
+
+            // if(thumbs[i].parentElement.className.includes("glide__slide")){
+            //     thumbs[i].parentElement.setAttribute('id','highlighted');
+            // }else{
+            //     thumbs[i].setAttribute('id','highlighted');                
+            // }
+
+
+            // for (let x = 0; x < solutionPanels.length; x++) {
+            //     const panel = solutionPanels[x];
+            //     if (thumbs[i].getAttribute("data-id") == panel.getAttribute("data-id")) {
+            //         document.getElementById("active").removeAttribute("id")
+            //         panel.setAttribute('id','active');
+            //     }
+            // }
         })
         console.log(thumbs[i])
     }
@@ -25,8 +33,12 @@ window.addEventListener("load", () => {
         perView: 4,
         // autoplay: 4000,
         // hoverPause: checkbox.checked,
-        // focusAt: 'center',
-        gap: 10,
+        focusAt: 'center',
+        // gap: 10,
+        // peek: {
+        //     before: 1000,
+        //     after: 100,
+        // },
         breakpoints: {
           800: {
             perView: 3
@@ -45,6 +57,33 @@ window.addEventListener("load", () => {
 
     glide.mount()
 
+    glide.on('run.after', function() {
+        active = document.querySelector(".glide__slide--active")
+        // console.log(active)
+        updateSlide(active.querySelector(".selector-thumb"))
+
+      })
+
     // new Glide('.glide').mount()
 
 });
+
+function updateSlide(thumb) {
+    
+    document.getElementById("highlighted").removeAttribute("id")
+
+    if(thumb.parentElement.className.includes("glide__slide")){
+        thumb.parentElement.setAttribute('id','highlighted');
+    }else{
+        thumb.setAttribute('id','highlighted');                
+    }
+
+
+    for (let x = 0; x < solutionPanels.length; x++) {
+        const panel = solutionPanels[x];
+        if (thumb.getAttribute("data-id") == panel.getAttribute("data-id")) {
+            document.getElementById("active").removeAttribute("id")
+            panel.setAttribute('id','active');
+        }
+    }
+}
